b7268c1fcac6c44c633cb9c24627b8de87e9f684
OnyiGlobal2025 <onyedikaokoro8@gmail.com>
2026-06-23 00:59:49 +0100

    iam: add eks:DescribeCluster and ACM perms; sync terraform config

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "onyiglobal-terraform-state-bucket" # Replace with your S3 bucket name
-    key            = "taskflow-infra/terraform.tfstate" # Path to store the state file in the bucket
+    key            = "taskflow-infra/terraform.tfstate"  # Path to store the state file in the bucket
     region         = "us-east-1"                         # The region of your S3 bucket
     encrypt        = true                                # Enable encryption for security
     dynamodb_table = "terraform-lock-table"              # The DynamoDB table for state locking
